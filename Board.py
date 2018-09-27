61093c2143cfb3a1c8a3934d6c1d8cbb44f57d76
ValentinKhanBlouki <42465969+ValentinKhanBlouki@users.noreply.github.com>
2018-09-27 21:56:19 +0200

    Update Board.py
    Korrekturvorschläge:
    - bei isMill() Klammern vor %8 bzw %3
    - bei checkMove() nur ungerade Ringnummern (Mittelsteine, nicht Ecksteine) für ringübergreifendes Ziehen erlauben
    - bei checkMove() gibt es bei Phase 1 keine Old Position --> =None in Schnittstelle? bin nicht ganz sicher

--- Board.py
+++ Board.py
@@ -19,40 +19,49 @@ class Board:
         else:
             #TODO
             pass
-
+    #---------------------
     def isMill(self, position):
         """Prüft, ob sich an angegebener Position eine Mühle befindet"""
         result = False
         color = (self.getStone(position)).getColor()
-        if position%2==1:
-            result = result or (self._stoneArray[position.getRing()][position.getNumber()-1 %8]==color 
-                                and self._stoneArray[position.getRing()][position.getNumber()+1 %8]==color)
-            result = result or (self._stoneArray[position.getRing()-1 %3][position.getNumber()]==color 
-                                and self._stoneArray[position.getRing()+1 %3][position.getNumber()]==color)
-        else:
-            result = result or (self._stoneArray[position.getRing()][position.getNumber()-2 %8]==color 
-                                and self._stoneArray[position.getRing()][position.getNumber()-1 %8]==color)
-            result = result or (self._stoneArray[position.getRing()][position.getNumber()+1 %8]==color 
-                                and self._stoneArray[position.getRing()][position.getNumber()+2 %8]==color)
-        return result
+ 
+        #Klammern ergänzt
+        if position.getNumber()%2==1:  #Mittelsteine mit Nummern 1,3,5,7
+            result = (self._stoneArray[(position.getRing()+1)%3][position.getNumber()]==color      
+                     and self._stoneArray[(position.getRing()-1)%3][position.getNumber()]==color)    #ringübergreifende Mühle
+                     or                                                                              #oder
+                     (self._stoneArray[position.getRing()] [(position.getNumber()+1)%8)]
+                     and (self._stoneArray[position.getRing()] [(position.getNumber()-1)%8)])        #Mühle mit benachbarten Ecksteinen
+            
+        else:                          #Ecksteine mit Nummern 0,2,4,6
+            result = (self._stoneArray[(position.getRing()][(position.getNumber()-1)%8]==color
+                     and self._stoneArray[(position.getRing()][(position.getNumber()-2)%8]==color)    #gegen den Uhrzeigersinn
+                     or
+                     (self._stoneArray[(position.getRing()][(position.getNumber()+1)%8]==color
+                     and self._stoneArray[(position.getRing()][(position.getNumber()+2)%8]==color)    #im Uhrzeigersinn
     
-
-    def checkMove(self,oldPosition, newPosition, phase):
-        """Guckt, ob der Stein von der alten zur neuen Position verschoben werden darf"""
-        if phase ==1 or phase==3:
-            if self._stoneArray[newPosition.getRing()][newPosition.getNumber()]=="":
+    #---------------------
+                                          
+    def checkMove(self,oldPosition=None, newPosition, phase):     #=None, falls bei Setzphase keine alte Position angegeben
+        """Prüft, ob der Stein auf ausgewähltes Feld kann"""
+        if phase ==1 or phase==3:  #Phase 1 --> Setzen bzw. Phase 3--> Springen
+            if self._stoneArray[newPosition.getRing()][newPosition.getNumber()]=="":    #nur prüfen, ob Feld frei
                 return True
             else:
                 return False
-        elif phase==2:
+                                          
+        elif phase==2:     #Phase 2 --> Zielen
             numberDistance = abs(oldPosition.getNumber()-newPosition.getNumber())
-            ringDistance = abs(oldPosition.retRing()-newPosition.getRing())
-            if ringDistance ==0 and (numberDistance==1 or numberDistance==7):
+            ringDistance = abs(oldPosition.getRing()-newPosition.getRing())
+                                          
+            if ringDistance ==0 and (numberDistance==1 or numberDistance==7): #benachbartes Feld auf Ring
                 return True
-            if ringDistance ==1 and numberDistance==0:
+            if oldposition.getRing()%2==1 and ringDistance ==1 and numberDistance==0:     #ringübergreifend benachbartes Feld
                 return True 
             return False
 
+    #------------------------                                          
+                                            
     def numberOfStones(self,color):
         counter = 0
         for i in range (0,2):
@@ -61,7 +70,9 @@ class Board:
                     if self._stoneArray[i][j].getColor == color:
                         counter+=1
         return counter
-
+                                          
+    #--------------------------
+                                          
     def saveBoardConfig(self):
         #speichert den aktuellen Zustand des Bretts ab und prüft, ob dieser schon zweimal erreicht wurde. In dem Fall True, sonst False
         #TODO
